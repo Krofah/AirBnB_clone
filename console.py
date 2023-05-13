097fca1cc2cd22fa904c93050d03522b0f831e3a
Adams2005 <chrisadamsadewuni@gmail.com>
2023-05-13 23:38:53 +0100

    Task 6

--- console.py
+++ console.py
@@ -5,38 +5,21 @@ from datetime import datetime
 import sys
 """ Class Definition class HBNBCommand(cmd.Cmd):"""
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb)'
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """Quit command  to exit the program"""
+        print("")
         return True
 
     def do_EOF(self, arg):
         """ EXit on EOF"""
         print("")
         return True
-    def emptyline(self, arg):
+    def emptyline(self):
         """an empty line + ENTER shouldn’t execute anything"""
         pass
+ 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
- 
-    def do_create(self):
-        """Instance that saves it (to json file) and print the id . EX:$"""
-        self.id = str(datetime.now().timestamps())
-if len(sys.argv) < 3:
-    print("** class name missing **")
-else:
-    class_name = sys.argv[2]
-    try:
-        if class_name not in globalS();
-        print("** class doesn't exist **")
-    else:
-        model = BaseModel()
-date = {"id",model.id}
-save_filr = open("BaseModel.json","w")
-json.dump(date ,save_file, indent = 6)
-save_file.close()
-print(model.id)
-except KeyError:
-    print
+
