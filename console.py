2362771f801b0e148329e5ddc3046de2675db3ee
Adams2005 <chrisadamsadewuni@gmail.com>
2023-05-14 21:08:58 +0100

    Pycodestyle

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ import cmd
 import json
 from datetime import datetime
 import sys
+import json
 
 
 class HBNBCommand(cmd.Cmd):
@@ -22,6 +23,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """an empty line + ENTER shouldn’t execute anything"""
         pass
+
+    def do_create(self, arg):
+        if arg:
+            if arg != "BaseModel":
+                print(" ** class doesn't exist ** ")
+            else:
+                instance_name = 'data.json'
+                with open(instance_name, "w") as json_file:
+                    json.dump(arg, json_file)
+                    print(id_name)
+        else:
+            print(" **class name missing** ")
 
 
 if __name__ == "__main__":
